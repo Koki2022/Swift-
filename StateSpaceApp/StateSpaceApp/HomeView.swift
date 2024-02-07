de8d322381950841dc44c2dda7e6cf7bc5c53390
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-02-08 08:45:02 +0900

    状態変数での画面遷移

--- StateSpaceApp/StateSpaceApp/HomeView.swift
+++ StateSpaceApp/StateSpaceApp/HomeView.swift
@@ -1,24 +1,29 @@
 //
 //  ContentView.swift
-//  StateSpaceApp
 //
-//  Created by 高橋昴希 on 2024/02/08.
+//  Created by 高橋昴希 on 2024/02/07.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    // お店情報のシートの状態を管理する状態を管理する変数
+    @State private var storeEditIsShowSheet: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // NavigationStackと配列パスの紐づけ
+        NavigationStack {
+            VStack {
+                Text("ホーム画面")
+                // ダミーリスト100個用意
+                List(1..<100) { list in
+                    NavigationLink("ダミー", destination: StoreInfoView(storeEditIsShowSheet: $storeEditIsShowSheet))
+                }
+            }
         }
-        .padding()
     }
 }
 
+
 #Preview {
-    ContentView()
+    HomeView()
 }
--- StateSpaceApp/StateSpaceApp/StoreEditView.swift
+++ StateSpaceApp/StateSpaceApp/StoreEditView.swift
@@ -1,18 +1,21 @@
 //
 //  StoreEditView.swift
-//  StateSpaceApp
 //
-//  Created by 高橋昴希 on 2024/02/08.
+//  Created by 高橋昴希 on 2024/02/07.
 //
 
 import SwiftUI
 
 struct StoreEditView: View {
+    // お店情報のシートの状態を管理する状態を管理する変数
+    @Binding var storeEditIsShowSheet: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            storeEditIsShowSheet.toggle()
+        }) {
+            Text("ホーム画面に戻る")
+        }
+        .navigationTitle("お店編集画面")
     }
 }
 
-#Preview {
-    StoreEditView()
-}
