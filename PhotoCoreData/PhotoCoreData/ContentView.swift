a321ba680d8b9be9f961fb0cba16029d3f9e3b24
高橋昴希 <takahashikoki@takahashisubarunozominoMacBook-Air.local>
2024-08-10 00:42:24 +0900

    CoreDataテスト

--- PhotoCoreData/PhotoCoreData/ContentView.swift
+++ PhotoCoreData/PhotoCoreData/ContentView.swift
@@ -10,6 +10,10 @@ import CoreData
 import PhotosUI
 
 struct ContentView: View {
+    // プロパティラッパー @FetchRequestで、データベースよりデータを取得
+    @FetchRequest(
+        entity: Photos.entity(), sortDescriptors: []
+    ) private var fetchedPhotos: FetchedResults<Photos>
     @Environment(\.managedObjectContext) private var viewContext
     @Environment(\.presentationMode) private var presentation
     // アイテムを複数枚保持する配列
@@ -17,13 +21,25 @@ struct ContentView: View {
     // PhotosPickerItem -> UIImageに変換した複数のアイテムを格納するプロパティ
     @State var selectedImages: [UIImage] = []
     //　ファイル名を格納する配列。
-    @State private var arrayFileName = [""]
+    @State private var arrayFileNames: [String] = []
+    
     var body: some View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
+                    // Photosエンティティのfilanameアトリビュートへアクセス
+                    ForEach(fetchedPhotos) { photo in
+                        // 文字列で結合されているfilenameをカンマ区切りで分解
+                        if let unwrappedFileName = photo.fileName?.components(separatedBy: ",") {
+                            let _ = print("CoreData: \(unwrappedFileName)")
+                            // 画面表示の際にCoreDataに保存したファイル名からUIImageを読み込む処理を実行
+                            // loadImage関数の引数に代入
+                        }
+                    }
                     // 配列内に画像が存在すれば表示
                     if !selectedImages.isEmpty {
+                        
+                        
                         // 選択された画像を表示
                         ForEach(selectedImages, id: \.self) { image in
                             PhotosPicker(selection: $selectedItems, selectionBehavior: .ordered) {
@@ -49,7 +65,7 @@ struct ContentView: View {
                         // 非同期処理
                         Task {
                             selectedImages = []
-                            arrayFileName = []
+                            arrayFileNames = []
                             for item in items {
                                 guard let data = try await item.loadTransferable(type: Data.self) else { continue }
                                 guard let uiImage = UIImage(data: data) else { continue }
@@ -59,8 +75,8 @@ struct ContentView: View {
                                 if let fileName = saveImageAndGetFileName(image: uiImage) {
                                     print("画像が保存されました。ファイル名: \(fileName)")
                                     // ファイル名を配列に格納
-                                    arrayFileName.append(fileName)
-                                    print(arrayFileName)
+                                    arrayFileNames.append(fileName)
+                                    print(arrayFileNames)
                                 } else {
                                     print("画像の保存に失敗しました")
                                 }
@@ -75,13 +91,13 @@ struct ContentView: View {
                 Text("保存")
             }
         }
-        
     }
+    
     // 追加ボタン押下した際にデータを保存する関数
     private func addPhotosItem() {
         let photos = Photos(context: viewContext)
         // ファイル名に格納している配列の値を文字列で結合
-        let fileNameString = arrayFileName.joined(separator: ",")
+        let fileNameString = arrayFileNames.joined(separator: ",")
         // データを保存
         photos.fileName = fileNameString
         // 生成したインスタンスをCoreDataに保存する
@@ -91,7 +107,6 @@ struct ContentView: View {
         } catch {
             print("ERROR \(error)")
         }
-        
     }
     // UIImageをストレージに保存し、ファイル名を返す関数
     private func saveImageAndGetFileName(image: UIImage) -> String? {
@@ -100,17 +115,21 @@ struct ContentView: View {
         let fileName = "\(uuid).png"
         
         // 画像をPNGデータに変換
-        guard let data = image.pngData() else { return nil }
+        guard let data = image.pngData() else {
+            return nil
+        }
         
         // ドキュメントディレクトリのURLを取得
-        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
         
         // 保存するファイルのフルパスを作成
-        let fileURL = documentsDirectory?.appendingPathComponent(fileName)
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
         
         do {
             // データをファイルに書き込む
-            try data.write(to: fileURL!)
+            try data.write(to: fileURL)
             
             // 保存したファイル名を返す
             return fileName
@@ -119,6 +138,18 @@ struct ContentView: View {
             return nil
         }
     }
+    // ファイル名を取り込みUIImage型のデータを返却する関数
+    func loadImage(fileName: String) -> UIImage? {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        
+        if let imageData = try? Data(contentsOf: fileURL) {
+            return UIImage(data: imageData)
+        }
+        
+        return nil
+    }
+    
 }
 
 #Preview {
