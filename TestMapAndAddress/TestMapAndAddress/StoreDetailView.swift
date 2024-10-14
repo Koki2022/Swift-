16bd092cf5653bc610e0fea2de92c350f59de50f
Koki2022 <kokikoki3219@icloud.com>
2024-10-15 07:09:06 +0900

    追加ボタン押下後と画面起動後のデータ確認コードを実装

---
+++ TestMapAndAddress/TestMapAndAddress/StoreDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailView.swift
+//  TestMapAndAddress
+//
+//  Created by 高橋昴希 on 2024/10/15.
+//
+
+import SwiftUI
+
+struct StoreDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreDetailView()
+}
